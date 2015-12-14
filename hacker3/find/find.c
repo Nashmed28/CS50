b96cfd957166e3485b897a75e3ac1d8a1077983e
Nashmed28 <nahmed3536@gmail.com>
2015-12-14 00:10:00 -0500

    Update find.c

--- hacker3/find/find.c
+++ hacker3/find/find.c
@@ -3,6 +3,9 @@
  *
  * Computer Science 50
  * Problem Set 3
+ * 
+ * Nabib Ahmed
+ * nahmed@college.harvard.edu
  *
  * Prompts user for as many as MAX values until EOF is reached, 
  * then proceeds to search that "haystack" of values for given needle.
